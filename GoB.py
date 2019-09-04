948ca7fdf7b470f8493cf430fc02b0f85e24ad4b
daniel.grauer <daniel.grauer@virtamed.com>
2019-09-05 00:11:36 +0200

    fixed mesh update input

--- GoB.py
+++ GoB.py
@@ -177,7 +177,7 @@ class GoB_OT_import(bpy.types.Operator):
             # useful for development when the mesh may be invalid.
             me.validate(verbose=True)
             # update mesh data after transformations to fix normals
-            me.update(calc_edges=True, calc_edges_loose=True, calc_loop_triangles=True)
+            me.update(calc_edges=True, calc_edges_loose=True)
 
             # if obj already exist do code below
             if objName in bpy.data.objects.keys():
