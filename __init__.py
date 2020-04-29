7cf83a012f45726e10ad72531a55b4e841ee7e05
daniel.grauer <daniel.grauer@virtamed.com>
2020-04-29 17:06:53 +0200

    version bump

--- __init__.py
+++ __init__.py
@@ -34,7 +34,7 @@ bl_info = {
     "name": "GoB",
     "description": "An unofficial GOZ-like addon for Blender",
     "author": "ODe, JoseConseco, Daniel Grauer",
-    "version": (3, 2, 0),
+    "version": (3, 2, 1),
     "blender": (2, 80, 0),
     "location": "In the info header",
     "wiki_url": "http://wiki.blender.org/index.php/Extensions:"
