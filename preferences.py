bc3294837facfd515febf69096be406e545abd9c
dg <>
2020-07-15 00:37:11 +0200

    enable buttons text by  default

--- preferences.py
+++ preferences.py
@@ -68,7 +68,7 @@ class GoBPreferences(AddonPreferences):
     show_button_text: BoolProperty(
         name="Show header buttons text",
         description="Enable this to show the import/export text of the header buttons",
-        default=False)        
+        default=True)        
     performance_profiling: BoolProperty(
         name="[Debug] Process durations",
         description="This is used to identiyfy slow code, note this will slow down your transfer if enabled!",
