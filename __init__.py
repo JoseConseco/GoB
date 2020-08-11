2f39941dfa92e6a1bc83ecf9ca6083927184e985
dg <dg>
2020-08-11 17:54:41 +0200

    tooltip updates; version increase ; updated documentation link

--- __init__.py
+++ __init__.py
@@ -34,11 +34,10 @@ bl_info = {
     "name": "GoB",
     "description": "An unofficial GOZ-like addon for Blender",
     "author": "ODe, JoseConseco, Daniel Grauer",
-    "version": (3, 4, 6),
+    "version": (3, 4, 7),
     "blender": (2, 83, 0),
     "location": "In the info header",
-    "wiki_url": "http://wiki.blender.org/index.php/Extensions:"
-                "2.6/Py/Scripts/Import-Export/GoB_ZBrush_import_export",
+    "wiki_url": "https://github.com/JoseConseco/GoB/wiki",                
     "tracker_url": "https://github.com/JoseConseco/GoB/issues/new",
     "category": "Import-Export"}
 
