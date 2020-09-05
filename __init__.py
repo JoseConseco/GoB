b91db9255b4764e977e4b08bd0efcd26de2e28f7
dg <dg>
2020-09-05 15:21:37 +0200

    avoid invalid path and reopen file browser again

--- __init__.py
+++ __init__.py
@@ -34,7 +34,7 @@ bl_info = {
     "name": "GoB",
     "description": "An unofficial GOZ-like addon for Blender",
     "author": "ODe, JoseConseco, Daniel Grauer",
-    "version": (3, 4, 82),
+    "version": (3, 4, 83),
     "blender": (2, 83, 0),
     "location": "In the info header",
     "wiki_url": "https://github.com/JoseConseco/GoB/wiki",                
