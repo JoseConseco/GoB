b2019201f92bbb296372f21693fcce317d1eef9e
dg <dg>
2021-01-03 16:00:55 +0100

    version increase for release

--- __init__.py
+++ __init__.py
@@ -34,7 +34,7 @@ bl_info = {
     "name": "GoB",
     "description": "An unofficial GOZ-like addon for Blender",
     "author": "ODe, JoseConseco, Daniel Grauer",
-    "version": (3, 4, 26),
+    "version": (3, 5, 0),
     "blender": (2, 83, 0),
     "location": "In the info header",
     "doc_url": "https://github.com/JoseConseco/GoB/wiki",                
