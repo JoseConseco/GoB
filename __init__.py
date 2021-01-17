8c3fb7bb689d680c3fd74cf4b9f1ec81e5370dd5
dg <>
2021-01-17 16:52:13 +0100

    only run zbrush import when objelist file contains objects

--- __init__.py
+++ __init__.py
@@ -34,7 +34,7 @@ bl_info = {
     "name": "GoB",
     "description": "An unofficial GOZ-like addon for Blender",
     "author": "ODe, JoseConseco, Daniel Grauer",
-    "version": (3, 5, 2),
+    "version": (3, 5, 3),
     "blender": (2, 83, 0),
     "location": "In the info header",
     "doc_url": "https://github.com/JoseConseco/GoB/wiki",                
