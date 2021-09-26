ac4b4881f4d02abaf70d00854f2ee80e192c61c8
Daniel Grauer <dg>
2021-09-26 23:12:58 +0200

    testing some patterns

--- GoB.py
+++ GoB.py
@@ -1230,7 +1230,26 @@ class GoB_OT_export(Operator):
         if not prefs().export_raw_object_names:  
             new_name = re.sub('[^\w\_\-]+', '_', obj.name)
         else:            
-            new_name = obj.name.replace('.0', '.C0_0')
+            #new_name = obj.name.replace('\.[0-9]', '.C0_0')
+            suffix_pattern = '\.(.*)'
+            found_string = re.search(suffix_pattern, obj.name)            
+            pattern = '.C0_'
+            if found_string:
+                #if no numbers in suffix do this
+                found_numbers = re.search('[0-9]', found_string.group())
+                if found_numbers:
+                    #print("found numbers", found_numbers.group())                    
+                    if not pattern in found_string.group():
+                        print(found_string.group())
+                        found_string = found_string.group().strip('.')
+                        new_name = re.sub(suffix_pattern, pattern + found_string, obj.name)
+                    else:
+                        return
+                else:
+                    #print("no numbers found", found_numbers)
+                    return                
+            else:
+                return
         
         if new_name == obj.name:
             return            
--- preferences.py
+++ preferences.py
@@ -181,10 +181,10 @@ class GoB_Preferences(AddonPreferences):
         description="Object names will not be changed when exporting if possible, this might cause some name changes in zbrush",
         default=True)
 
-    export_rename_pattern: StringProperty(
+    """ export_rename_pattern: StringProperty(
         name = "Fix Suffix",
         description = 'Zbrush has a limitation to use a .0 in Tool names. To work around this a String needs to be added between the dot and the 0 (example: .0 -> .T0)',
-        default = 'C0_')
+        default = 'C0_') """
 
     export_merge: BoolProperty(
         name="Merge Vertices of Curves, Surfaces, Fonts and Meta Objects",
@@ -363,7 +363,7 @@ class GoB_Preferences(AddonPreferences):
         col.prop(self, 'export_remove_internal_faces')
         col.prop(self, 'export_raw_object_names')        
         """ if self.export_raw_object_names:
-            col.prop(self, 'export_rename_pattern') """        
+            col.prop(self, 'export_rename_pattern')  """   
         
         
         # GoB Import Options
