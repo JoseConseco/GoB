289ef4a10e82dd95c0ac3a5d676257e25f6c6543
Daniel G <1472884+kromar@users.noreply.github.com>
2024-10-05 16:33:17 +0200

    add options to flip the uv map when importing or exporting (#495)
    Co-authored-by: Daniel Grauer <dg>

--- __init__.py
+++ __init__.py
@@ -31,7 +31,7 @@ bl_info = {
     "description": """GoB (for GoBlender) is an unofficial GoZ-like extension, providing a seamless bridge between ZBrush and Blender. 
           Effortlessly transfer your models between ZBrush and Blender with a single click, streamlining your workflow and maximizing efficiency.""",
     "author": "ODe, JoseConseco, Daniel Grauer",
-    "version": (4, 1, 1),
+    "version": (4, 1, 2),
     "blender": (4, 00, 0),
     "location": "In the info header",
     "doc_url": "https://github.com/JoseConseco/GoB/wiki",                
