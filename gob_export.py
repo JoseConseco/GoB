794734c03694192a58844464da7dcce2944e152f
1029920178@qq.com <1029910278@qq.com>
2025-08-19 08:53:55 +0800

    improve verts/polygon data export performance: verts 400% faster; polygon 200% faster(using numpy)

--- gob_export.py
+++ gob_export.py
@@ -18,6 +18,7 @@
 
 import bpy
 import os
+import numpy as np
 import time
 import shutil
 from struct import pack
@@ -152,10 +153,24 @@ class GoB_OT_export(Operator):
             goz_file.write(pack('<4B', 0x11, 0x27, 0x00, 0x00))
             goz_file.write(pack('<I', numVertices*3*4+16))
             goz_file.write(pack('<Q', numVertices))
-            for vert in mesh_tmp.vertices:
-                modif_coo = obj.matrix_world @ vert.co      # @ is used for matrix multiplications
-                modif_coo = mat_transform @ modif_coo
-                goz_file.write(pack('<3f', modif_coo[0], modif_coo[1], modif_coo[2]))                
+            
+            vertex_coords = np.zeros(numVertices * 3, dtype=np.float32)
+            mesh_tmp.vertices.foreach_get('co', vertex_coords)
+            vertex_coords = vertex_coords.reshape(-1, 3)
+
+            matrix_world_np = np.array(obj.matrix_world, dtype=np.float32)
+            mat_transform_np = np.array(mat_transform, dtype=np.float32)
+            
+            homogeneous_coords = np.column_stack([vertex_coords, np.ones(numVertices)])
+            
+            # matrix_world
+            transformed_coords = (matrix_world_np @ homogeneous_coords.T).T[:, :3]
+            
+            # mat_transform
+            homogeneous_transformed = np.column_stack([transformed_coords, np.ones(numVertices)])
+            final_coords = (mat_transform_np @ homogeneous_transformed.T).T[:, :3]
+
+            goz_file.write(pack(f'<{numVertices * 3}f', *final_coords.flatten()))
 
             if utils.prefs().performance_profiling: 
                 start_time = utils.profiler(start_time, "Write Vertices")            
@@ -164,17 +179,28 @@ class GoB_OT_export(Operator):
             goz_file.write(pack('<4B', 0x21, 0x4E, 0x00, 0x00))
             goz_file.write(pack('<I', numFaces*4*4+16))
             goz_file.write(pack('<Q', numFaces))
-            for face in mesh_tmp.polygons:
-                if len(face.vertices) == 4:
-                    goz_file.write(pack('<4I', face.vertices[0],
-                                face.vertices[1],
-                                face.vertices[2],
-                                face.vertices[3]))
-                elif len(face.vertices) == 3:
-                    goz_file.write(pack('<3I4B', face.vertices[0],
-                                face.vertices[1],
-                                face.vertices[2],
-                                0xFF, 0xFF, 0xFF, 0xFF))
+            
+            face_vertices = np.zeros(numFaces * 4, dtype=np.int32)
+            mesh_tmp.polygons.foreach_get('vertices', face_vertices)
+            face_vertices = face_vertices.reshape(-1, 4)
+            
+            face_vertex_counts = np.zeros(numFaces, dtype=np.int32)
+            mesh_tmp.polygons.foreach_get('loop_total', face_vertex_counts)
+            
+            quad_faces = face_vertex_counts == 4
+            tri_faces = face_vertex_counts == 3
+            
+            if np.any(quad_faces):
+                quad_indices = face_vertices[quad_faces]
+                quad_data = quad_indices.flatten()
+                goz_file.write(pack(f'<{len(quad_data)}I', *quad_data))
+            
+            if np.any(tri_faces):
+                tri_indices = face_vertices[tri_faces]
+                for i in range(len(tri_indices)):
+                    goz_file.write(pack('<3I4B', tri_indices[i][0], tri_indices[i][1], 
+                                      tri_indices[i][2], 0xFF, 0xFF, 0xFF, 0xFF))
+            
             if utils.prefs().performance_profiling: 
                 start_time = utils.profiler(start_time, "Write Faces")
 
